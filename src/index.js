a0875b397da025c3b96c0a53f07b15aabcea1e38
glozano <ghlm81@hotmail.com>
2021-08-31 15:04:19 +0200

    Configuring the Rate pairs from arguments, environment variables and config file Adding dependency to nconf for arguments setting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 
 import { getRateFor } from "./Helpers/rate-retriever.js";
 import { RateTracker } from "./Helpers/rate-tracker.js";
+import nconf from 'nconf';
 
-const btcUsdTracker = new RateTracker('BTC-USD');
-const upXauUsdTracker = new RateTracker('UPXAU-USD');
+// Reading first config from params, then fron environment and last from config.json
+nconf.argv().env('__').file('src/Config/config.json');
+
+var currencyPairsParam = nconf.get('currencyPairs');
+if (!currencyPairsParam)
+{
+  console.log('No currency pairs defined, nothing to do');
+  process.exit(0);
+}
+var currencyPairs = currencyPairsParam.split(',');
+var currencyPairsRateTrackers = [];
+for (const currencyPair of currencyPairs)
+{
+  console.log(`Currency pair: ${currencyPair}`);
+  currencyPairsRateTrackers.push(new RateTracker(currencyPair));
+}
 
 // lets run this every 5 seconds
 const intervalObj = setInterval(async () => {
   try {
-    btcUsdTracker.process(await getRateFor('BTC-USD'));
-    upXauUsdTracker.process(await getRateFor('UPXAU-USD'));
+    for (const currencyPairRateTracker of currencyPairsRateTrackers)
+    {
+      currencyPairRateTracker.process(await getRateFor(currencyPairRateTracker.ticker));
+    }
   } catch (error)
   {
     console.log('Error while getting rates and processing them: ', error);
