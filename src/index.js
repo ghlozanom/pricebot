4b7150c3e1e92a781d2819e33856d2988a7a9283
glozano <ghlm81@hotmail.com>
2021-08-31 11:01:06 +0200

    Getting the ticker price in 5 seconds interval

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 
 import { getRateFor } from "./Helpers/rate-retriever.js";
 
-(async () => {
-
+async function getRateInIntervals(ticker) 
+{
   try {
     // Locate a card that can be used as the source for the transaction.
-    const btcUsdRate = await getRateFor('BTC-USD');
+    const btcUsdRate = await getRateFor(ticker);
 
-    // Create a transaction and log its outputs
-    console.log(`BTC-USD rate: ${btcUsdRate.ask}`);
+    console.log(`BTC-USD rate: ${btcUsdRate.ask} at ${new Date().toISOString()}`);
   } catch (error) {
       console.log('Error while calculating price oscilation, please try again');
     return;
   }
-})();
+}
+
+const intervalObj = setInterval(() => {
+  getRateInIntervals('BTC-USD');
+}, 1000 * 5); // five minutes
