c81e62edbafce7194b63b7908ba7557b75ed8a01
glozano <ghlm81@hotmail.com>
2021-08-31 12:16:19 +0200

    Creating a rate tracker showing a change in the price

---
+++ src/Helpers/rate-tracker.js
@@ -0,0 +1,21 @@
+
+export class RateTracker 
+{
+    process(lastPrice)
+    {
+        if (!this.basePrice)
+        {
+            this.basePrice = lastPrice;
+        }
+
+        const percentageDifference = this.basePrice * 0.0001;
+        const priceChange = Math.abs(lastPrice - this.basePrice);
+        var enoughChange = priceChange >= percentageDifference;
+        console.log(`Percentage change: ${priceChange}, percentage difference: ${percentageDifference}, enough: ${enoughChange}`);
+        
+        if ( enoughChange )
+        {
+            console.log('Price changed');
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 
 import { getRateFor } from "./Helpers/rate-retriever.js";
+import { RateTracker } from "./Helpers/rate-tracker.js";
+
+const rateTracker = new RateTracker();
 
 async function getRateInIntervals(ticker) 
 {
   try {
-    // Locate a card that can be used as the source for the transaction.
+    
     const btcUsdRate = await getRateFor(ticker);
 
     console.log(`BTC-USD rate: ${btcUsdRate.ask} at ${new Date().toISOString()}`);
+    rateTracker.process(btcUsdRate.ask);
+
   } catch (error) {
       console.log('Error while calculating price oscilation, please try again');
     return;
   }
 }
 
+// lets run this every 5 seconds
 const intervalObj = setInterval(() => {
   getRateInIntervals('BTC-USD');
-}, 1000 * 5); // five minutes
+}, 1000 * 5);
